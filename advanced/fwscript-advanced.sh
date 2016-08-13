508c8cf89a3450db6f57ed2dfd50d2f69e68a2b3
Katsuhiro Kamiya <k.kamiya.344@gmail.com>
2016-08-13 14:04:27 +0900

    update advanced/fwscript-advanced.sh to restrict user connection from gateway

--- advanced/fwscript-advanced.sh
+++ advanced/fwscript-advanced.sh
@@ -33,7 +33,7 @@ iptables -A INPUT -i ${LAN_IF} -p udp --sport 67:68 --dport 67:68 -j ACCEPT
 iptables -A INPUT -i ${LAN_IF} -s ${LAN_IP} -p tcp -m multiport --dports 22,53,80,3128 -j ACCEPT
 iptables -A INPUT -i ${LAN_IF} -s ${LAN_IP} -p udp -m multiport --dports 22,53,80,3128 -j ACCEPT
 
-# only ssh and http(s) are allowed
+# only ssh from WAN is allowed
 iptables -A INPUT -i ${WAN_IF} -p tcp --dport 22 -j ACCEPT
 
 # accept forward request from LAN ip range
@@ -45,3 +45,10 @@ iptables -t nat -A POSTROUTING -s ${LAN_IP} -o ${WAN_IF} -j MASQUERADE
 # never forgive you
 iptables -I INPUT -m mac --mac-source 50:7b:9d:90:1c:c1 -j DROP
 iptables -I FORWARD -m mac --mac-source 50:7b:9d:90:1c:c1 -j DROP
+
+# restrict outbound connection from normal user
+iptables -A OUTPUT -p tcp -m owner --uid-owner 500-60000 ! --dport 22 -j DROP
+iptables -A OUTPUT -p tcp -m owner --uid-owner 500:60000 -m limit --limit 10/s --limit-burst 10 -j ACCEPT
+iptables -A OUTPUT -p tcp -m owner --uid-owner 500:60000 -j DROP
+iptables -A OUTPUT -p udp -m owner --uid-owner 500:60000 -j DROP
+
